b6f39b9b43c86238ab3cab7f5da866f0f5fdfbf9
srujangsk <155732688+srujangsk@users.noreply.github.com>
2024-02-16 07:26:50 +0000

    ok

--- lyrics_to_html.py
+++ lyrics_to_html.py
@@ -11,13 +11,14 @@ def create_html_file(file_path, lyrics, log_file, main_heading):
         first_line = re.sub('ప\s*:\s*', '', first_line)
     
     html_file_path = os.path.join("vignapana_atmeeeya_geethalu", f"{first_line}.html")
+    song_name = first_line.replace(" ", "").replace(".html", "")
     
     with open(html_file_path, 'w') as file:
-        file.write(f"<html><head><meta name='viewport' content='width=device-width, initial-scale=1.0'><style>.lyrics {{ font-size: 18px; }}@media (max-width: 600px) {{.lyrics {{ font-size: 16px;}}</style></head><body style='background-color: #F0F0F0'><h1>{main_heading}</h1><h2>{first_line}</h2><pre class='lyrics'>{lyrics}</pre></body></html>")
+        file.write(f"<html><head><meta name='viewport' content='width=device-width, initial-scale=1.0'></head><body style='background-color: #F0F0F0'><h1>{main_heading}</h1><h2>{first_line}</h2><pre class='lyrics'>{lyrics}</pre></body></html>")
     
     log_file.write(f"{file_path} converted to {html_file_path}\n")
     
-    return f"vignapana_atmeeeya_geethalu/{first_line}.html"
+    return f"vignapana_atmeeeya_geethalu/{song_name}.html"
 
 def create_index_file(song_links):
     index_file_path = "index.html"    
@@ -26,7 +27,7 @@ def create_index_file(song_links):
         file.write("<html><head><meta name='viewport' content='width=device-width, initial-scale=1.0'></head><body><h1>విజ్ఞాపన ఆత్మీయ గీతాలు</h1>")
         for link in song_links:
             song_heading = link.split('/')[-1].split('.')[0]  
-            file.write(f"<p><a href='{link}.html'>{song_heading}</a></p>")
+            file.write(f"<p><a href='{link}' style='text-decoration: none;'>{song_heading}</a></p>")
         file.write("</body></html>")
 
 def main():
@@ -44,7 +45,8 @@ def main():
                 song_link = create_html_file(file_path, lyrics, log_file, main_heading)
                 song_links.append(song_link)
     
-    create_index_file(song_links)
+    created_song_links = [f"{link}.html" for link in song_links]
+    create_index_file(created_song_links)
     log_file.close()
 
 if __name__ == "__main__":
